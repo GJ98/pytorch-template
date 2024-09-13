c1caa7e5e6fd284367c78934edae1774ae4fb223
gityeop <freiheit517@icoud.com>
2024-09-13 22:56:11 +0900

    Refactor configuration setting and model saving path

--- base/base_trainer.py
+++ base/base_trainer.py
@@ -8,6 +8,7 @@ class BaseTrainer:
     """
     Base class for all trainers
     """
+
     def __init__(self, model, criterion, metric_ftns, optimizer, config):
         self.config = config
 
@@ -16,13 +17,18 @@ class BaseTrainer:
         self.metric_ftns = metric_ftns
         self.optimizer = optimizer
 
-        cfg_trainer = config['trainer']
-        self.epochs = cfg_trainer['epochs']
-        self.mode = config['trainer']['mode']
-        self.best_score = float('inf') if self.mode == "min" else 0
+        cfg_trainer = config["trainer"]
+        self.epochs = cfg_trainer["epochs"]
+        self.mode = config["trainer"]["mode"]
+        self.best_score = float("inf") if self.mode == "min" else 0
         if not os.path.exists(config["trainer"]["save_dir"]):
             os.makedirs(config["trainer"]["save_dir"])
-        self.save_file = f'{config["trainer"]["save_dir"]}'+f'{self.config["arch"]["type"]}_{self.config["arch"]["args"]["plm_name"]}'.replace("/", "-")
+        self.save_file = (
+            f'{config["trainer"]["save_dir"]}'
+            + f'{self.config["arch"]["type"]}_{self.config["arch"]["args"]["plm_name"]}'.replace(
+                "/", "-"
+            )
+        )
 
     @abstractmethod
     def _train_epoch(self, epoch):
@@ -37,7 +43,9 @@ class BaseTrainer:
         """
         Full training logic
         """
-        wandb.init(project=self.config['wandb']['project_name'], name=self.save_file.split("/")[-1])
+        wandb.init(
+            project=self.config["wandb"]["project_name"],
+            name=self.save_file.split("/")[-1],
+        )
         for epoch in range(self.epochs + 1):
             self._train_epoch(epoch)
-
